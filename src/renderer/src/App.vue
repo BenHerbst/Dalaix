0a5d47477fafc43c64b68bff3e7683df7105c4ee
Ben Herbst <ben.hbst@gmail.com>
2023-03-25 00:32:02 +0100

    Basic data binding

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,50 +1,63 @@
-<script setup>
-</script>
-
 <template>
-  <div class="container mt-5 shadow-lg rounded-3 p-lg-5 p-3 p-sm-4 border">
+  <div class="container my-5 shadow-lg rounded-3 p-5 border">
     <h2 class="fw-bold">Dalaix by Ben Herbst</h2>
     <p>Install Dalaix the simple and easy way.
       Just fill out this form and press install!
       <br />
-      Start and stop Dalai via the Start menu entries</p>
+      Start and stop Dalai via the Start menu entries
+    </p>
     <h3>Model</h3>
     <p class="form-label">Select model:</p>
-    <select class="mb-3 form-select form-control">
+    <select v-model="selected" class="mb-3 form-select form-control">
       <option selected>None</option>
-      <option value="7B">7B Model</option>
-      <option value="13B">13B Model</option>
-      <option value="30B">30B Model</option>
-      <option value="65B">65B Model</option>
+      <option>7B Model</option>
+      <option>13B Model</option>
+      <option>30B Model</option>
+      <option>65B Model</option>
     </select>
     <h3>Dependencies</h3>
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="installDocker" id="installDockerCheck">
+      <input class="form-check-input" type="checkbox" v-model="installDocker" id="installDockerCheck">
       <label class="form-check-label" for="installDockerCheck">Install Docker?</label>
     </div>
     <div class="form-check mb-3">
-      <input class="form-check-input" type="checkbox" value="installNPM" id="installNPMCheck">
+      <input class="form-check-input" type="checkbox" v-model="installNpm" id="installNPMCheck">
       <label class="form-check-label" for="installNPMCheck">Install Npm?</label>
     </div>
     <h3>Run</h3>
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="startMenuRun" id="startMenuRunCheck">
+      <input class="form-check-input" type="checkbox" id="startMenuRunCheck" v-model="runEntry">
       <label class="form-check-label" for="startMenuRunCheck">Run entry in start menu?</label>
     </div>
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="startMenuStop" id="startMenuStopCheck">
+      <input class="form-check-input" type="checkbox" id="startMenuStopCheck" v-model="stopEntry">
       <label class="form-check-label" for="startMenuStopCheck">Stop entry in start menu?</label>
     </div>
     <div class="form-check mb-3">
-      <input class="form-check-input" type="checkbox" value="autostart" id="autostartCheck">
+      <input class="form-check-input" type="checkbox" id="autostartCheck" v-model="autostart">
       <label class="form-check-label" for="autostartCheck">Autostart entry for Dalai?</label>
     </div>
     <div class="d-flex flex-md-row flex-column">
-      <button class="btn btn-primary px-5 endButton mb-md-0 mb-2" id="sendButton">Install Dalai</button>
+      <button class="btn btn-primary px-5 endButton mb-md-0" id="sendButton">Install Dalai</button>
       <button class="ms-md-2 btn btn-outline-secondary px-3 endButton" id="cancelButton">Cancel</button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: "None",
+      autostart: false,
+      runEntry: true,
+      stopEntry: true,
+      installDocker: true,
+      installNpm: true
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import './assets/css/styles.scss';
