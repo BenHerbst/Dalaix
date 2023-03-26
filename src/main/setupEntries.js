f46f5388b2d826bb9ac6945fbc2349e38c9b244a
Ben Herbst <ben.hbst@gmail.com>
2023-03-26 16:47:41 +0200

    Fix start and stop entries

--- src/main/setupEntries.js
+++ src/main/setupEntries.js
@@ -5,27 +5,27 @@ export default function setupEntries(dalaiFolder, autostart, runEntry, stopEntry
     if (runEntry) {
         // start entry
         const fs = require('fs');
-        try { fs.writeFileSync('C:\\Dalai\\start.bat', 'cd ' + dalaiFolder + ' && npx dalai && msg * Starting Daila Server', 'utf-8'); }
+        try { fs.writeFileSync('C:\\Dalai\\start.bat', 'cd ' + dalaiFolder + ' && msg * Starting Daila Server && npx dalai serve', 'utf-8'); }
         catch (e) {
-            alert('Failed to save the file !');
+            alert('Failed to save the start batch !');
         }
     }
 
     if (autostart) {
         // start entry
         const fs = require('fs');
-        try { fs.writeFileSync('C:\\Dalai\\autostart.bat', 'cd ' + dalaiFolder + ' && npx dalai', 'utf-8'); }
+        try { fs.writeFileSync('C:\\Dalai\\autostart.bat', 'cd ' + dalaiFolder + ' && npx dalai serve', 'utf-8'); }
         catch (e) {
-            alert('Failed to save the file !');
+            alert('Failed to save the autostart batch !');
         }
     }
 
     if (stopEntry) {
         // stop entry
         const fs = require('fs');
-        try { fs.writeFileSync('C:\\Dalai\\start.bat', 'cd ' + dalaiFolder + ' && npx dalai && msg * Stopping Daila Server', 'utf-8'); }
+        try { fs.writeFileSync('C:\\Dalai\\stop.bat', 'cd ' + dalaiFolder + ' && msg * Stopping Daila Server && npx dalai serve', 'utf-8'); }
         catch (e) {
-            alert('Failed to save the file !');
+            alert('Failed to save the stop batch !');
         }
     }
 }
